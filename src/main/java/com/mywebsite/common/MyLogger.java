f93e971e1a81fc10bd99892c23dbee661ab7e345
Kasperx <Titango2011@gmail.com>
2022-07-22 22:49:28 +0200

    logger works

--- src/main/java/com/mywebsite/common/MyLogger.java
+++ src/main/java/com/mywebsite/common/MyLogger.java
@@ -1,16 +1,23 @@
 package main.java.com.mywebsite.common;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.config.properties.PropertiesConfiguration;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import javax.naming.InitialContext;
+
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
- 
+
 public class MyLogger {
     
-    static Logger log;
-    
-    Logger getLogger()
-    {
-        return log;
-    }
+    static Logger logger;
+    static {init();}
     public static Logger getLogger(String name)
     {
         return LogManager.getLogger(name);
@@ -19,4 +26,27 @@ public class MyLogger {
     {
         return LogManager.getLogger(name);
     }
+    /**
+	 * method to init log4j configurations
+	 */
+	private static void init()
+	{
+		try {
+	        System.setProperty(
+	        		"log4j.configuration",
+	        		new File(System.getProperty("user.dir")+"/src/main/java/com/mywebsite/props/log4j.properties")
+	        		.toURI()
+	        		.toURL()
+	        		.toString()
+	        		);
+//	        BasicConfigurator.configure();
+	        Configurator.initialize(new DefaultConfiguration());
+	        Configurator.setRootLevel(Level.INFO);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		// or
+		// -Dlog4j.configuration=file:src/main/java/com/mywebsite/props/log4j.properties
+	}
+
 }
