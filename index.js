6f99401ccd0efc8f756d5828824a234208327bfc
Christian Glass <cgl@ekf.de>
2022-08-10 08:12:27 +0200

    json

--- index.js
+++ index.js
@@ -50,10 +50,147 @@ function login()
 }
 
 function encodeQueryData(data) {
-    const ret = [];
-    for (let d in data)
-    {
-      ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+  const ret = [];
+  for (let d in data)
+  {
+    ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
     }
     return ret.join('&');
+}
+function getData()
+{
+  $.ajax({
+    url: "?get=data",
+    context: document.body
+  }).done(function(data) {
+      $('#h1').empty();
+      $('#h1').append('<h1 style="font-size:50px;"><marquee>User view</marquee></h1></p>');
+      $('#table').remove();
+      let sizeOfData = data.length;
+      // create table with data
+      var table = $('<table>').addClass('table').addClass('table-striped');
+      table.attr('id', 'table');
+      // table.append('<tr>');
+      // table.append('<th>ID</th>');
+      // table.append('<th>FIRSTNAME</th>');
+      // table.append('<th>LASTNAME</th>');
+      // table.append('<th>STATE</th>');
+      // table.append('<th>AGE</th>');
+      // table.append('<th>PW</th>');
+      table.append('<tr>');
+      table.append('<th>'+data[0][0]+'</th>');
+      table.append('<th>'+data[0][1]+'</th>');
+      table.append('</tr>');
+      for(let i=1; i<data.length; i++){
+        table.append('<tr>');
+        table.append('<th>'+data[i][0]+'</th>');
+        table.append('<th>'+data[i][1]+'</th>');
+        table.append('</tr>');
+      }
+      // $.each(data, function(key, val)
+      // {
+      //     table.append('<tr>');
+      //     table.append('<td>'+val.id+'</td>');
+      //     table.append('<td>'+val.firstname+'</td>');
+      //     table.append('<td>'+val.lastname+'</td>');
+      //     table.append('<td>'+val.state+'</td>');
+      //     table.append('<td>'+val.age+'</td>');
+      //     table.append('<td>'+val.pw+'</td>');
+      //     table.append('</tr>');
+      // });
+      table.append('</table>');
+      $('body').append('<p></p>');
+      $('body').append(table);
+      // $('#table').append('<p></p>');
+      // $('#table').append(table);
+      let text;
+      if(sizeOfData == undefined)
+      {
+        text='';
+      }else{
+        text=' ('+sizeOfData+')';
+      }
+      // $("#input").html('Got data'+text).addClass('btn btn-success');
+    });
+}
+function getWeather()
+{
+  $.ajax({
+    url: "?get=weather",
+    context: document.body
+  }).done(function(data) {
+    });
+}
+function getAllData()
+{
+  $.ajax({
+    url: "?get=admin&user=admin&pw=secret",
+    context: document.body
+  }).done(function(data) {
+      $('#table').remove();
+      let sizeOfData = data.length;
+      // create table with data
+      $('#h1').empty();
+      $('#h1').append('<h1 style="font-size:50px;"><marquee>Admin view</marquee></h1></p>');
+      var table = $('<table>').addClass('table').addClass('table-striped');
+      table.attr('id', 'table');
+      // table.append('<tr>');
+      // table.append('<th>ID</th>');
+      // table.append('<th>FIRSTNAME</th>');
+      // table.append('<th>LASTNAME</th>');
+      // table.append('<th>STATE</th>');
+      // table.append('<th>AGE</th>');
+      // table.append('<th>PW</th>');
+      // table.append('</tr>');
+      // $.each(data, function(key, val)
+      // {
+      //     table.append('<tr>');
+      //     table.append('<td>'+val.id+'</td>');
+      //     table.append('<td>'+val.firstname+'</td>');
+      //     table.append('<td>'+val.lastname+'</td>');
+      //     table.append('<td>'+val.state+'</td>');
+      //     table.append('<td>'+val.age+'</td>');
+      //     table.append('<td>'+val.pw+'</td>');
+      //     table.append('</tr>');
+      // });
+      table.append('<tr>');
+      table.append('<th>'+data[0][0]+'</th>');
+      table.append('<th>'+data[0][1]+'</th>');
+      table.append('</tr>');
+      for(let i=1; i<data.length; i++){
+        table.append('<tr>');
+        table.append('<th>'+data[i][0]+'</th>');
+        table.append('<th>'+data[i][1]+'</th>');
+        table.append('<th>'+data[i][2]+'</th>');
+        table.append('<th>'+data[i][3]+'</th>');
+        if(data[i][4] == 0){
+          table.append('<th>no</th>');
+        } else {
+          table.append('<th>yes</th>');
+        }
+        table.append('</tr>');
+      }
+      table.append('</table>');
+      $('body').append('<p></p>');
+      $('body').append(table);
+      // $('#table').append('<p></p>');
+      // $('#table').append(table);
+      let text;
+      if(sizeOfData == undefined)
+      {
+        text='';
+      }else{
+        text=' ('+sizeOfData+')';
+      }
+      // $("#input").html('Got data'+text).addClass('btn btn-success');
+    });
+}
+function insertData()
+{
+  $.ajax({
+    url: "?get=insert",
+    context: document.body
+  }).done(function(data) {
+    alert("Done");
+  });
 }
